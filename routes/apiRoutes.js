1194a40d75c9433f7b51bde054743624e56e804b
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-03-28 18:52:00 -0500

    changes for passport and populating data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,51 +8,77 @@ module.exports = function (app) {
   //======================================================
   //Passport Route
   //======================================================    
-  app.post('/login',
-    passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      failureFlash: true
-    })
+  app.post("/api/login", passport.authenticate('local', {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+  })
   );
+
+  app.post("/api/register", function (req, res) {
+    var hashedPW = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+    db.User.findOne({
+      where: {
+        [Op.or]: [
+          {
+            username: req.body.username
+          },
+          {
+            email: req.body.email
+          }
+        ]
+      }
+    }).then(function (user) {
+      if (user) {
+        res.redirect("/register")
+      }
+      else {
+        db.User.create({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPW
+        })
+        res.redirect("/login")
+      }
+    })
+  });
 
   //======================================================
   //Get candidate info and answers - not using mysql
   //======================================================  
-app.get("/api/users"), function(req,res) {
-  var currentUser = req.body;
+  app.get("/api/users"), function (req, res) {
+    var currentUser = req.body;
 
-  for (var i=0; i < candidatesArr.length; i++){
-    for (var j=0; j < currentUser.positions.length; j++){
-      if (candidatesArr[i] === currentUser.positions[i]){
-        score ++
-      } 
-      bestMatch = candidatesArr[i]; 
+    for (var i = 0; i < candidatesArr.length; i++) {
+      for (var j = 0; j < currentUser.positions.length; j++) {
+        if (candidatesArr[i] === currentUser.positions[i]) {
+          score++
+        }
+        bestMatch = candidatesArr[i];
+      }
+    };
+
+    for (var j = 0; j < currentUser.score.length; j++) {
+      console.log(currentUser.score[j]);
+    };
+
+    for (var k = 0; k < candidatesArr.length; k++) {
+      console.log(candidatesArr[k].score);
     }
-  };
 
-  for (var j = 0; j<currentUser.score.length; j++) {
-    console.log(currentUser.score[j]);
-  };
-
-  for (var k=0; k<candidatesArr.length;k++){
-    console.log(candidatesArr[k].score);
   }
 
-}
+  app.get("/api/questions", function (req, res) {
+    res.json(candidatesArr);
+  })
 
-app.get("/api/questions", function(req, res){
-  res.json(candidatesArr);
-})
+  app.get("/api/users", function (req, res) {
+    res.json(usersArr);
+  })
 
-app.get("/api/users", function(req, res){
-  res.json(usersArr);
-})
+  app.post("/api/users", function (req, res) {
+    res.json(usersArr);
+  })
 
-app.post("/api/users", function(req, res){
-  res.json(usersArr);
-})
-  
   //======================================================
   //Quiz Route(s) using mysql
   //======================================================  
