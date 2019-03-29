cfd77cc14172aa55613d000964906b73df68a06f
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-03-29 16:01:38 -0500

    [candidate_position_schema.sql , candidate_position_seeds.sql] updated the schema from position to answer and changed the seeds

---
+++ db/candidate_position_schema.sql
@@ -0,0 +1,12 @@
+USE 2020_electionsDB;
+
+CREATE TABLE candidate_position (
+  id INT NOT NULL AUTO_INCREMENT,
+  issue VARCHAR(50) NULL,
+  question VARCHAR(300) NULL,
+  position VARCHAR(100) NULL,
+  first_name VARCHAR(50) NULL,
+  last_name VARCHAR(50) NULL,
+  candidate VARCHAR (100) NULL,  
+  PRIMARY KEY (id)
+);
