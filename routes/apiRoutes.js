32f7b664e42138e66deaf58124964e55e9e4cb3b
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-03-29 19:33:26 -0500

    moved all the HTML files and made handlebars file for all candidates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-var db = require("../models"); 
+var db = require("../models");
 var passport = require("passport");
 // // var candidatesArr = require("../app/data/candidates");
 // var usersArr = require("../app/data/users");
@@ -43,25 +43,74 @@ module.exports = function (app) {
   });
 
   //======================================================
+  // Individual Candidate Route(s) using mysql
+  //====================================================== 
+
+  app.get("/api/candidate", function (req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Candidate.findAll({}).then(function (dbCandidate) {
+      res.json(dbCandidate);
+    });
+  });;
+
+  app.get("/api/candidate/:id", function (req, res) {
+    db.Candidate.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbCandidate) {
+      res.json(dbCandidate);
+      console.log(dbCandidate);
+    });
+  });
+
+  app.post("/api/candidate", function (req, res) {
+    db.Candidate.create(req.body).then(function (dbCandidate) {
+      res.json(dbCandidate);
+    });
+  });
+
+  app.delete("/api/candidate/:id", function (req, res) {
+    db.Candidate.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbCandidate) {
+      res.json(dbCandidate);
+    });
+  });
+
+  //======================================================
+  //All Candidates Handlebar File 
+  //======================================================  
+
+
+
+
+
+
+  //======================================================
   //Quiz Route(s) using mysql
   //======================================================  
   // Get all examples
-  app.get("/api/questions", function(req, res) {
-    db.Quiz.findAll({}).then(function(dbQuestions) {
+  app.get("/api/questions", function (req, res) {
+    db.Quiz.findAll({}).then(function (dbQuestions) {
       res.json(dbQuestions);
     });
   });
 
   // Create a new example
-  app.post("/api/questions", function(req, res) {
-    db.Quiz.create(req.body).then(function(dbQuestions) {
+  app.post("/api/questions", function (req, res) {
+    db.Quiz.create(req.body).then(function (dbQuestions) {
       res.json(dbQuestions);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/questions/:id", function(req, res) {
-    db.dbQuestions.destroy({ where: { id: req.params.id } }).then(function(dbQuestions) {
+  app.delete("/api/questions/:id", function (req, res) {
+    db.dbQuestions.destroy({ where: { id: req.params.id } }).then(function (dbQuestions) {
       res.json(dbQuestions);
     });
   });
