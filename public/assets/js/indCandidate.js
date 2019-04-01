da9f31f29b5e1dbba7a37a4f5891d1525d7c44b4
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-04-01 11:10:46 -0500

    [indCandidate.js] only displays a # of articles

--- public/assets/js/indCandidate.js
+++ public/assets/js/indCandidate.js
@@ -1,12 +1,12 @@
 $(function () {
 
-    function getCandidate() {
-        $.get("/api/candidate/:id", function (data) {
-            console.log(data);
-            console.log()
-        });
-    };
-    getCandidate();
+    // function getCandidate() {
+    //     $.get("/api/candidate/:id", function (data) {
+    //         console.log(data);
+    //         console.log()
+    //     });
+    // };
+    // getCandidate();
 
     function newsAPI() {
         var dataId = $("#newsAPI").attr("data-id");
@@ -16,7 +16,7 @@ $(function () {
             dataId
             + '&' +
             'from=2019-03-24&' +
-            'sortBy=recent&' +
+            'sortBy=recent&pageSize=3&' +
             'apiKey=400435e5285343fca12fc6110393eab5';
 
         var req = new Request(url);
@@ -35,10 +35,6 @@ $(function () {
                     var newsOutlet = (articles[i].source.name);
                     var title = (articles[i].title);
                     var articleUrl = (articles[i].url);
-
-                    console.log("News Outlet: " + newsOutlet);
-                    console.log("Title: " + title);
-                    console.log("URL: " + articleUrl);
 
                     //updated HTML 
                     $("#newsAPI").prepend("News Outlet: " + newsOutlet + "<br>" + "Title: " + title + "<br>" + "URL: " + articleUrl + "<br><br>");
@@ -62,15 +58,18 @@ $(function () {
             dataType: 'json',
             headers: { 'X-API-Key': 'ujXwLjfkOR6Pof7IUaTKVR1MCoWuu9o7Q8iHT4om' }
         }).done(function (data) {
-            console.log(data);
-            console.log("Name: " + data.results[0].name);
-            console.log("Date coverage from: " + data.results[0].date_coverage_from);
-            console.log("Date coverage to: " + data.results[0].date_coverage_to);
-            console.log("Independent expenditures : $" + data.results[0].independent_expenditures);
-            console.log("Total Contributions: $" + data.results[0].total_contributions);
-            console.log("Total Disbursements: $" + data.results[0].total_disbursements);
-            console.log("Total from Individuals: $" + data.results[0].total_from_individuals);
-            console.log("Total from PACs: $" + data.results[0].total_from_pacs);
+            // console.log(data);
+            // console.log("Name: " + data.results[0].name);
+            // console.log("Date coverage from: " + data.results[0].date_coverage_from);
+            // console.log("Date coverage to: " + data.results[0].date_coverage_to);
+            // console.log("Independent expenditures : $" + data.results[0].independent_expenditures);
+            // console.log("Total Contributions: $" + data.results[0].total_contributions);
+            // console.log("Total Disbursements: $" + data.results[0].total_disbursements);
+            // console.log("Total from Individuals: $" + data.results[0].total_from_individuals);
+            // console.log("Total from PACs: $" + data.results[0].total_from_pacs);
+
+            //updated HTML 
+            $("#financeAPI").prepend("Total from PACs: $" + data.results[0].total_from_pacs+ "<br><br>");
         });
     }
     financeAPI();
